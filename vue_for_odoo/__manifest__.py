464fb5469a691a5ce56f65ad833f0e9ef14177be
x2-team <yuze@odoo-dl.com>
2024-04-28 10:59:29 +0800

    init

---
+++ vue_for_odoo/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Vue3 and Element-plus Assets",
+    'summary': """
+        Vue3 and Element-plus assets
+    """,
+    'description': """
+        Using Vue3 and Element-plus in Odoo
+    """,
+    'author': "x2soft",
+    'website': "www.odoo-dl.com",
+    'support': "yuze@odoo-dl.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'assets': {
+        'web.assets_backend': [
+            'vue_for_odoo/static/lib/vue/*',
+            'vue_for_odoo/static/lib/element-ui/**/*',
+        ],
+    },
+}
